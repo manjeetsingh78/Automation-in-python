396d868bb3a1e2d4e41cc5ee941ee7244c85a929
Manjeet Singh <rambaghel741791@gmail.com>
2026-03-23 01:19:15 +0530

    Data retrievel file has been uploaded

--- SQL Database/DataRetrievel.py
+++ SQL Database/DataRetrievel.py
@@ -1,2 +1,25 @@
 import sqlite3
+# Establish a connection and create a cursor
+con = sqlite3.connect('database.db')
+cur = con.cursor()
 
+#Exceute sql
+
+cur.execute("SELECT * FROM 'ips' ORDER BY ASN")
+print(cur.fetchall())
+
+cur.execute("SELECT address, asn FROM 'ips' ORDER BY ASN")
+print(cur.fetchall())
+
+cur.execute("SELECT address, asn FROM 'ips' WHERE asn < 300")
+print(cur.fetchall())
+
+cur.execute("SELECT address, asn FROM 'ips' where asn = 140")
+print(cur.fetchall())
+
+cur.execute("SELECT address, asn FROM 'ips' where asn < 300 and domain like '%sa'")
+result = cur.fetchall()
+print(result)
+
+result2 = cur.fetchall()
+print(result2)
