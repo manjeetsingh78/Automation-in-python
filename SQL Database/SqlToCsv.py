8677fd52cc57783b9149881dfb427e44642c86e4
Manjeet Singh <rambaghel741791@gmail.com>
2026-03-23 01:50:46 +0530

    SQLITE3 Functionality has been learnt

--- SQL Database/SqlToCsv.py
+++ SQL Database/SqlToCsv.py
@@ -8,4 +8,5 @@ df = pandas.read_sql_query("SELECT * FROM 'ips' ORDER BY asn",con)
 print(df)
 
 # df.to_csv('database.csv',index=None)
+# if we are using pandas then we should use pandas read sql function instead of cursor fetch all
 df.to_excel('database.xlsx',index=None)
